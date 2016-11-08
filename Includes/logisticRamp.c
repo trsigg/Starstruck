6adbc015cf7dce23472616cd2f2fdad34d0a795a
Tynan Sigg <trsigg@gmail.com>
2016-11-08 14:57:17 -0700

    $: Bugfixes and adding wheelie winch
    Still untested

--- Includes/logisticRamp.c
+++ Includes/logisticRamp.c
@@ -3,11 +3,11 @@ typedef struct {
 } logisticRamper;
 
 void initializeLogisticRamp(logisticRamper *ramper, float k=1, float M=1, float intercept = 0.25) {
-  ramper.k = k;
-  ramper.M = M;
-  ramper.C = M / intercept - 1;
+  ramper->k = k;
+  ramper->M = M;
+  ramper->C = M / intercept - 1;
 }
 
 float logisticRampRuntime(logisticRamper *ramper, float input) {
-  return ramper.M / (1 + ramper.C * exp(-ramper.k * ramper.M * input));
+  return ramper->M / (1 + ramper->C * exp(-ramper->k * ramper->M * input));
 }
