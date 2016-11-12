23793ca914b97eaa1b742ba049bc1ad246d2f7cf
Ethan Nunes <ethan.nunes@att.net>
2016-11-11 21:31:00 -0700

    FIxed Autonomous
    Redid the autonomous so that it would actually work, we now have for
    Right Side, Left Side, and Programming Skills, however the potentiometer
    is flaky.

--- (G-team/(2131G.c
+++ (G-team/(2131G.c
@@ -1,6 +1,7 @@
 #pragma config(Sensor, in1,    gyro,           sensorGyro)
 #pragma config(Sensor, in2,    liftPot,        sensorPotentiometer)
 #pragma config(Sensor, in3,    clawPot,        sensorPotentiometer)
+#pragma config(Sensor, in4,    modePot,        sensorPotentiometer)
 #pragma config(Sensor, dgtl1,  leftEnc,        sensorQuadEncoder)
 #pragma config(Sensor, dgtl3,  rightEnc,       sensorQuadEncoder)
 #pragma config(Motor,  port1,           rbd,           tmotorVex393_HBridge, openLoop, reversed)
@@ -36,10 +37,11 @@
 #define liftBottom 346
 #define liftMax 2310
 #define liftTop 1820
-#define liftPush 1462
-#define clawClosedPos 4096-3595
+#define liftPush 1495+260
+#define clawClosedPos 4096-3250 //3595
 #define clawOpenPos 4096-2595
 #define clawMax 4096-1200
+#define clawPush 4096-1860
 
 //Constants
 #define liftStillSpeed 10
@@ -77,10 +79,16 @@ void pre_auton() {
 //autonomous region
 void deploy(int waitAtEnd=250) {
 	//deploy stops
-	goToPosition(lift, liftMax);
-	goToPosition(claw, clawMax, clawStillSpeed);
-	goToPosition(lift, liftBottom + 500);
-	goToPosition(claw, clawOpenPos);
+
+	//OLD DEPLOY
+	//goToPosition(lift, liftMax);
+	//goToPosition(claw, clawMax, clawStillSpeed);
+	//goToPosition(lift, liftBottom + 500);
+	//goToPosition(claw, clawOpenPos);
+
+  driveStraight(10);
+  driveStraight(-7);
+
 }
 
 void setClawStateManeuver(bool open = !clawOpen) { //toggles by default
@@ -105,60 +113,135 @@ void hyperExtendClaw(bool stillSpeed=true) {
 	goToPosition(claw, clawMax, (stillSpeed ? clawStillSpeed : 0));
 }
 
-task pillowAuton() { //Put true to run 2 things at once
+task pillowAutonRight() { //Put true to run 2 things at once
 	setClawStateManeuver(true); //open claw
   //goToPosition(claw, clawOpenPos);
 	openClaw();
-  driveStraight(10); //drive away from wall
+  driveStraight(7); //drive away from wall
   //while(driveData.isDriving);
 
   //move toward pillow
-  turn(-0.95);
+  turn(-45.5, false, 40, 120, -40);
   //while(turnData.isTurning || claw.maneuverExecuting);
-  driveStraight(8);
+  driveStraight(18);
 
   //goToPosition(claw, clawClosedPos, ); //clamp pillow
   closeClaw();
 
-  turn(0.6);
-  //while(turnData.isTurning);
   goToPosition(lift, liftTop);
 
+  turn(30.5, false, 40, 120, -10);
+  //while(turnData.isTurning);
+  //goToPosition(lift, liftTop);
+
   //--lift up--//
-  driveStraight(6);
+  driveStraight(13);
   //while (driveData.isDriving);
-  turn(33, true, 40, 80, -20); //turn to face fence
+  //turn(0.4, false, 40, 80, -20); //turn to face fence
   //while (turnData.isTurning);
-  driveStraight(17, true); // drive up to wall
+  driveStraight(10, true); // drive up to wall
   //while (driveData.isDriving || lift.maneuverExecuting); //Changed shoulder to lift; WORK?
 
   openClaw(); //release pillow
   closeClaw();
-  driveStraight(-5); //back up
-  hyperExtendClaw();
+  driveStraight(-10); //back up
+  //turn(15.5, false, 40, 120, -10);
+  goToPosition(lift, liftPush);
+  goToPosition(claw, clawPush, clawStillSpeed);
 
   //push jacks over
- 	driveStraight(10);
- 	goToPosition(claw, 900);
+ 	driveStraight(13);
+ 	driveStraight(-15);
+ 	//goToPosition(claw, 900);
 
- 	createManeuver(claw, clawMax, clawStillSpeed); //hyperextend claw
+ 	//Push off Stars
+ 	turn(-30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+ 	turn(30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+
+ 	closeClaw();
+
+ 	//createManeuver(claw, clawMax, clawStillSpeed); //hyperextend claw
 
  	//drive to other wall and lift down
- 	driveStraight(-5); //Put True if you want to perform two tasks at once
+ //	driveStraight(-5); //Put True if you want to perform two tasks at once
  	//while (driveData.isDriving);
- 	turn(63);
+ 	//turn(63);
  	//while (turnData.isTurning || claw.maneuverExecuting);
- 	createManeuver(lift, liftMax, liftStillSpeed);
- 	driveStraight(43);
+ 	//createManeuver(lift, liftMax, liftStillSpeed);
+ 	//driveStraight(43);
  	//while (driveData.isDriving || lift.maneuverExecuting);
- 	turn(-80, false, 40, 120, -40);
- 	driveStraight(7);
+ 	//turn(-80, false, 40, 120, -40);
+ 	//driveStraight(7);
 
- 	goToPosition(lift, liftPush); //push jacks over
+ 	//goToPosition(lift, liftPush); //push jacks over
 }
 
-/*task oneSideAuton() {
-	createManeuver(claw, clawOpen, clawStillSpeed); //open claw   CHANGED TO CLAW OPEN
+task pillowAutonLeft() { //Put true to run 2 things at once
+	setClawStateManeuver(true); //open claw
+  //goToPosition(claw, clawOpenPos);
+	openClaw();
+  driveStraight(7); //drive away from wall
+  //while(driveData.isDriving);
+
+  //move toward pillow
+  turn(45.5, false, 40, 120, -40);
+  //while(turnData.isTurning || claw.maneuverExecuting);
+  driveStraight(18);
+
+  //goToPosition(claw, clawClosedPos, ); //clamp pillow
+  closeClaw();
+
+  goToPosition(lift, liftTop);
+
+  turn(-30.5, false, 40, 120, -10);
+  //while(turnData.isTurning);
+  //goToPosition(lift, liftTop);
+
+  //--lift up--//
+  driveStraight(12);
+  //while (driveData.isDriving);
+  //turn(0.4, false, 40, 80, -20); //turn to face fence
+  //while (turnData.isTurning);
+  driveStraight(10, true); // drive up to wall
+  //while (driveData.isDriving || lift.maneuverExecuting); //Changed shoulder to lift; WORK?
+
+  openClaw(); //release pillow
+  closeClaw();
+  driveStraight(-10); //back up
+  //turn(15.5, false, 40, 120, -10);
+  goToPosition(lift, liftPush);
+  goToPosition(claw, clawPush, clawStillSpeed);
+
+  //push jacks over
+ 	driveStraight(13);
+ 	driveStraight(-13);
+ 	//goToPosition(claw, 900);
+
+ 	//Push off Stars
+ 	turn(-30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+ 	turn(30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+
+ 	//createManeuver(claw, clawMax, clawStillSpeed); //hyperextend claw
+
+ 	//drive to other wall and lift down
+ //	driveStraight(-5); //Put True if you want to perform two tasks at once
+ 	//while (driveData.isDriving);
+ 	//turn(63);
+ 	//while (turnData.isTurning || claw.maneuverExecuting);
+ 	//createManeuver(lift, liftMax, liftStillSpeed);
+ 	//driveStraight(43);
+ 	//while (driveData.isDriving || lift.maneuverExecuting);
+ 	//turn(-80, false, 40, 120, -40);
+ 	//driveStraight(7);
+
+ 	//goToPosition(lift, liftPush); //push jacks over
+}
+ task programmingSkills() {
+	/*createManeuver(claw, clawOpen, clawStillSpeed); //open claw   CHANGED TO CLAW OPEN
 	createManeuver(lift, liftTop, liftStillSpeed); //lift to near top
   driveStraight(5, true); //drive away from wall
   while(driveData.isDriving);
@@ -173,8 +256,67 @@ task pillowAuton() { //Put true to run 2 things at once
   while (turnData.isTurning || lift.maneuverExecuting);  //CHANGED SHOULDER TO LIFT; WORK?
 
   driveStraight(25);
-  goToPosition(lift, 2100); //CHANGED TO OUR VALUES?
-} */
+  goToPosition(lift, 2100); //CHANGED TO OUR VALUES? */
+
+  //OLD SINGLE SIDE AUTON ^^^^^^^^^^^^^^^
+
+  setClawStateManeuver(true);
+	openClaw();  //Open The Claw
+  driveStraight(7); //drive away from wall
+
+  //move toward pillow
+  turn(-45.5, false, 40, 120, -40);
+  driveStraight(18);
+
+  closeClaw(); //Grab Pillow
+
+  goToPosition(lift, liftTop); //Rise Lift
+
+  //Angle to Wall
+  turn(30.5, false, 40, 120, -10);
+  driveStraight(13);
+
+  driveStraight(10, true); // drive up to wall
+
+  //Relase Claw and Prepare to Push
+  openClaw(); //release pillow
+  closeClaw();
+  driveStraight(-10); //back up
+  goToPosition(lift, liftPush);
+  goToPosition(claw, clawPush, clawStillSpeed);
+
+  //Push and Backup
+ 	driveStraight(12);
+ 	driveStraight(-20); //reverse from wall to turn
+
+ 	//Continue to Push off Stars
+ 	turn(-30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+ 	turn(30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+
+ 	driveStraight(-20); //reverse from wall to turn again
+
+ 	//Another Round Of Pushing Off Stars
+ 	turn(-30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+ 	turn(30.5, false, 40, 120, -10);
+ 	driveStraight(15);
+
+ 	//Reverse To Try and Grab The Pillow and Drop It ****** NEEDS TUNING
+ 	driveStraight(-15);
+ 	turn(95.5, false, 40, 120, -10);
+ 	goToPosition(lift, liftBottom);
+ 	driveStraight(13);
+ 	goToPosition(claw, clawClosedPos);
+ 	driveStraight(-15);
+ 	turn(90, false, 40, 120, -10);
+ 	goToPosition(lift, liftTop);
+ 	driveStraight(30);
+ 	goToPosition(claw, clawOpenPos);
+ 	driveStraight(-15);
+
+}
 
 task autonomous() {
 	lift.maneuverExecuting = false;
@@ -186,12 +328,14 @@ task autonomous() {
   //autoSign = (SensorValue[sidePot] < 1800) ? 1 : -1;
 
   //start appropriate autonomous task
-  //if (SensorValue[modePot] > 2540) {
-  	startTask(pillowAuton);
-  //} else if (SensorValue[modePot] > 1320) {
-  //	startTask(oneSideAuton);
+  if (SensorValue[modePot] >= 0 && SensorValue[modePot] < 9) {
+  	startTask(pillowAutonLeft);
+  } else if (SensorValue[modePot] > 10 && (SensorValue[modePot] < 30)) {
+  	startTask(programmingSkills);
+  } else if (SensorValue[modePot] > 35) {
+  	startTask(pillowAutonRight);
   }
-//}
+}
 //end autonomous region
 
 void clawControl() {
